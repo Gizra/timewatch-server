5292aecbd9c7b37155f73f8341832be5b82bad74
Itamar Shapira Bar-Lev <bitamar@gmail.com>
2015-11-22 19:49:48 +0200

    Timewatch module

---
+++ timewatch/modules/custom/timewatch_session/plugins/restful/work_sessions/1.0/TimewatchWorkSessionsResource.class.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * Contains TimewatchWorkSessionsResource.
+ */
+
+class TimewatchWorkSessionsResource extends \RestfulEntityBaseNode {
+
+  /**
+   * Overrides \RestfulEntityBaseNode::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['start'] = array(
+      'property' => 'field_session_date',
+      'sub_property' => 'value',
+      'process_callbacks' => array('intval'),
+    );
+
+    $public_fields['end'] = array(
+      'property' => 'field_session_date',
+      'sub_property' => 'value2',
+      'process_callbacks' => array('intval'),
+    );
+
+    $public_fields['employee'] = array(
+      'property' => 'field_employee',
+      'sub_property' => 'name',
+    );
+
+    return $public_fields;
+  }
+}
